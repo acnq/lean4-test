04f9a21c9c6519398d832389c94dd6cc0fa98923
acnq <fanyuanhaotc@163.com>
2023-12-20 21:27:34 +0200

    git renewal

--- p4/8_syntax_extension/8.4_MacroOverview/8.4_Macro_overview.lean
+++ p4/8_syntax_extension/8.4_MacroOverview/8.4_Macro_overview.lean
@@ -7,28 +7,28 @@ macro_rules
   | `({$x, $xs:term,*}) => `(Set.insert $x {$xs,*})
 /- Provided `Set` has been imported (from Mathlib4), these are all we need for `{1, 2, 3}` to be valid notation to create a literal set -/
 
-open Lean 
-inductive Syntax where 
+open Lean
+inductive Syntax where
   | missing : Syntax
-  | node (kind : SyntaxNodeKind) (args : Array Syntax) : Syntax 
+  | node (kind : SyntaxNodeKind) (args : Array Syntax) : Syntax
   | atom : String -> Syntax
-  | ident : Name -> Syntax 
+  | ident : Name -> Syntax
 abbrev MacroM := ReaderT Macro.Context (EStateM Macro.Exception Macro.State)
 structure Context where
-  methods       : MethodsRef 
-  mainModule    : Name 
+  methods       : MethodsRef
+  mainModule    : Name
   currMacroScope: MacroScope
   currRecDepth  : Nat := 0
   maxRecDepth   : Nat := defaultMaxRecDepth
   ref           : Lean.Syntax
 
-inductive Exception where 
+inductive Exception where
   | error             : Lean.Syntax → String → Exception
   | unsupportedSyntax : Exception
 
-structure State where 
+structure State where
   macroScope : MacroScope
-  traceMsgs  : List (Prod Name String) := List.nil 
-  deriving Inhabited 
+  traceMsgs  : List (Prod Name String) := List.nil
+  deriving Inhabited
 
-  
+/- test whether the git is OK?-/
